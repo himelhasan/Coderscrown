8461f872baee5085a475280e5905be2900d77dd3
himelhasan <himelhasan497@gmail.com>
2023-08-12 18:02:33 +0600

    user post route created

---
+++ src/app/api/users/signup/route.ts
@@ -0,0 +1,39 @@
+import { connect } from "@/dbConfig/dbConfig";
+import User from "@/models/userSignUp/userSignUp.Model";
+import { NextRequest, NextResponse } from "next/server";
+import bcryptjs from "bcryptjs";
+
+connect();
+
+export const POST = async (request: NextRequest) => {
+  try {
+    const reqBody = await request.json();
+    const { username, email, password } = reqBody;
+    console.log(reqBody);
+
+    // check if user already exists
+    const user = await User.find({ email });
+
+    if (user) {
+      return NextResponse.json({ error: "User already exists" }, { status: 400 });
+    }
+
+    //    hash the password
+    const salt = await bcryptjs.genSalt(10);
+    const hashedPassword = await bcryptjs.hash(password, salt);
+
+    //   create a new user
+    const newUser = new User({ username, email, password: hashedPassword });
+    const savedUser = await newUser.save();
+
+    console.log(savedUser);
+    // send response
+    return NextResponse.json({
+      message: "User created successfully",
+      success: true,
+      savedUser,
+    });
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+};
